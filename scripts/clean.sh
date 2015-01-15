6b85b14a88a30dfd161e7a0abe840b2f66a208c3
Nick Bargnesi <nbargnesi@selventa.com>
2015-01-15 16:45:41 -0500

    add production build capabilities

---
+++ scripts/clean.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# The next three lines are for the go shell.
+export SCRIPT_NAME="Clean"
+export SCRIPT_HELP="Clean the build."
+[[ "$GOGO_GOSH_SOURCE" -eq 1 ]] && return 0
+
+# Normal script execution starts here.
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
+source "$DIR"/env.sh || exit 1
+source "$SCRIPTS"/functions.sh || exit 1
+
+assert_env BUILD
+rm -fr "$BUILD" || exit 1
+
