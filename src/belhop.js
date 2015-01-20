24b9aded49039e76999aef95996abc0c90374537
Nick Bargnesi <nbargnesi@selventa.com>
2015-01-20 18:52:41 -0500

    temporary jsdoc'd, node'd library

--- src/belhop.js
+++ src/belhop.js
@@ -1,3 +1,60 @@
+/**
+ * @file BELHop JavaScript library.
+ * @author OpenBEL Committers
+ * @license Apache-2.0
+*/
+(function() {
+
+  var root = this;
+
+  /**
+   * The BELHop module.
+   * @exports belhop
+   * @author Nick Bargnesi <nbargnesi@selventa.com>
+   * @version 0.1.0
+   */
+  var belhop = function(obj) {
+    if (obj instanceof belhop) return obj;
+    if (!(this instanceof belhop)) return new belhop(obj);
+    belhop.init = true;
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = belhop;
+  } else {
+    root.belhop = belhop;
+  }
+
+  /**
+   * @name VERSION
+   * @readonly
+   * @type {string}
+   * @default
+   */
+  Object.defineProperty(belhop, 'VERSION', {
+    get: function() { return '0.1.0'; }
+  });
+
+  /**
+   * Executes a completion on some input and returns the results.
+   * @arg {object} completion - BEL API completion object.
+   * @arg {string} input - BEL expression to autocomplete.
+   */
+  belhop.complete = function(completion, input) {
+
+  };
+
+}.call(this));
+
+/*
+ * Represents a book.
+ * @constructor
+ * @arg {string} title - The title of the book.
+ * @arg {string} author - The author of the book.
+ *
+function Book(title, author) {
+}
+
 window.BH = (function() {
   function BELHop() {
 
@@ -18,13 +75,12 @@ window.BH = (function() {
       }
     },
 
-    complete: {
-      statement: function() {
-        return {}
-      },
-      term: function() {
-        return {}
-      }
+    /
+     * Executes a completion on some input and returns the results.
+     * @arg {object} completion - BEL API completion object.
+     * @arg {string} input - BEL expression to autocomplete.
+     *
+    complete: function(completion, input) {
     },
 
     validate: {
@@ -38,3 +94,4 @@ window.BH = (function() {
   };
   return belhop;
 }());
+*/
