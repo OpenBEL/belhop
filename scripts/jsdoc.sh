3e0958b56450654a00fed866a5ebb5c5a695bea0
Nick Bargnesi <nbargnesi@selventa.com>
2015-01-20 18:38:10 -0500

    add jsdoc support
    Currently just dumping to the out directory.

---
+++ scripts/jsdoc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# The next three lines are for the go shell.
+export SCRIPT_NAME="JSDoc"
+export SCRIPT_HELP="Generate API documentation via JSDoc."
+[[ "$GOGO_GOSH_SOURCE" -eq 1 ]] && return 0
+
+# Normal script execution starts here.
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
+source "$DIR"/env.sh || exit 1
+source "$SCRIPTS"/functions.sh || exit 1
+assert_env NPM_MODPATH
+
+# Create the node environment if needed...
+create_node_env || exit 1
+# ... and use it.
+export PATH="$NPM_MODPATH/.bin":$PATH
+
+cd "$DIR" || exit 1
+require_cmd "jsdoc"
+#jsdoc --package package.json $(find src -name "*.js") --verbose
+jsdoc --readme docs/readme src --verbose
+
