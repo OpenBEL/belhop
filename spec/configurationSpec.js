c4772d3a3ccde77fe07e1704ce8a9d7adb1bcc31
Nick Bargnesi <nbargnesi@selventa.com>
2015-01-24 14:29:46 -0500

    expand spec tests to cover current API

--- spec/configurationSpec.js
+++ spec/configurationSpec.js
@@ -1,23 +1,28 @@
-describe("belhop", function () {
+describe('belhop', function () {
 
-  describe("configuration", function() {
+  describe('configuration', function() {
 
-    it("has a default API URL", function() {
+    it('has a default API URL', function() {
       expect(belhop.DEFAULT_URL).toBeDefined();
     });
 
-    it("can get the API URL", function() {
+    it('can get the API URL', function() {
       expect(belhop.configuration.getURL()).toBeDefined();
     });
 
-    it("defaults to using the default API URL", function() {
+    it('defaults to using the default API URL', function() {
       expect(belhop.configuration.getURL()).toEqual(belhop.DEFAULT_URL);
     });
 
-    it("can change API URL", function() {
-      var newURL = "http://api.openbel.org/bel";
+    it('can change API URL', function() {
+      var newURL = 'http://api.openbel.org/bel';
       belhop.configuration.setURL(newURL);
       expect(belhop.configuration.getURL()).toEqual(newURL);
+    });
+
+    it('can reset to the default API URL', function() {
+      belhop.configuration.setURL(null);
+      expect(belhop.configuration.getURL()).toEqual(belhop.DEFAULT_URL);
     });
 
   });
