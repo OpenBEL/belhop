1a20cf48281424087bdb7fdf7a0c249b9e1eb4bc
Nick Bargnesi <nbargnesi@selventa.com>
2015-01-24 14:26:18 -0500

    start gutting demo
    These 100 lines of code will get replaced by library functionality.

--- demo/bel-autocomplete.js
+++ demo/bel-autocomplete.js
@@ -13,102 +13,6 @@ $(document).ready(function() {
     '<p>{{value}}<span class="completion-type">{{displayType}}</p>';
 
   /**
-   * Tokenize the query before sending it to the API; currently only one token
-   * is sent: the query itself.
-   */
-  var tokenizer = function(query) {
-    lastQuery = query;
-    return [query];
-  };
-
-  /**
-   * Manipulate the URL prior to generating an autocompletion.
-   */
-  var replacer = function(url, query) {
-    var ret = url.replace('%QUERY', query);
-    var end = $('#expinput')[0].selectionEnd;
-    ret += ('?caret_position=' + end);
-    return ret;
-  };
-
-  /**
-   * Filters the API response into typeahead datums for use in the completion
-   * template.
-   */
-  var responseFilter = function(response) {
-    var datums = [];
-    function insertDatum(element) {
-      datum = {
-        value: applyActions(element.completion.actions, lastQuery),
-        displayType: displayCompletionType(element.completion.type),
-        label: element.completion.label,
-        actions: element.completion.actions
-      };
-      datums.push(datum);
-    }
-    response.forEach(insertDatum);
-    return datums;
-  };
-
-  var belExpressions = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-    queryTokenizer: tokenizer,
-    prefetch: 'data/templates.json',
-    remote: {
-      url: 'http://next.belframework.org/api/expressions/%QUERY/completions',
-      replace: replacer,
-      filter: responseFilter
-    }
-  });
-
-  belExpressions.initialize();
-
-  /**
-   * Delete the characters from startPos to endPos, inclusively, and return the
-   * result.
-   */
-  function deleteAction(str, startPos, endPos) {
-    var str1 = str.substr(0, startPos);
-    var str2 = str.substr(endPos + 1);
-    var ret = str1 + str2;
-    return ret;
-  }
-
-  /**
-   * Insert the string value at a position in str.
-   */
-  function insertAction(str, value, position) {
-    var str1 = str.substr(0, position);
-    var str2 = value;
-    var str3 = str.substr(position);
-    var rslt = str1 + str2 + str3;
-    return rslt;
-  }
-
-  /**
-   * Apply autocomplete actions to some input and return the result.
-   */
-  function applyActions(actions, input) {
-
-    /* applies a single action */
-    function actOn(action) {
-      if (action.delete) {
-        var startPos = action.delete.start_position;
-        var endPos = action.delete.end_position;
-        input = deleteAction(input, startPos, endPos);
-      } else if (action.insert) {
-        var value = action.insert.value;
-        var position = action.insert.position;
-        input = insertAction(input, value, position);
-      }
-    }
-
-    /* apply each action, mutating input */
-    actions.forEach(actOn);
-    return input;
-  }
-
-  /**
    * Called when the user selects a completion from our dropdown.
    */
   function selected(event, datum, name) {
@@ -129,10 +33,14 @@ $(document).ready(function() {
     }
   };
 
+  function doQuery(query, cb) {
+    cb([{value: "$19.95", displayType: "Plus Shipping and Handling"}]);
+  };
+
   $('#bel-expressions .typeahead').typeahead(null, {
     name: 'bel-expressions',
     displayKey: 'value',
-    source: belExpressions.ttAdapter(),
+    source: doQuery,
     templates: {
       empty: [
         '<div class="empty-message">',
