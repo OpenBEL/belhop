17e6057f605d9f08b54050d83db8325bb9a4b80e
Nick Bargnesi <nbargnesi@selventa.com>
2015-02-10 18:14:50 -0500

    improve callback mechanics and spec
    Allow no-op'ing success and errors functions explicitly.

--- spec/callbackSpec.js
+++ spec/callbackSpec.js
@@ -4,8 +4,57 @@ describe('belhop', function() {
 
     it('can be produced', function() {
       expect(belhop.factory.callback).toBeDefined();
-      // TODO more
+      expect(typeof belhop.factory.callback).toBe('function');
+
+      function success() {}
+      function error() {}
+      var cb = belhop.factory.callback(success, error);
+
+      expect(cb).toBeDefined();
+      expect(cb).not.toBeNull();
+
+      expect(cb.success).toBe(success);
+      expect(cb.error).toBe(error);
     });
+
+    it('expose success/error functions', function() {
+      function success() {}
+      function error() {}
+      var cb = belhop.factory.callback(success, error);
+
+      expect(typeof cb.success).toBe('function');
+      expect(typeof cb.error).toBe('function');
+    });
+
+    it('call user-provided functions', function() {
+      var successCalled = false;
+      function success() { successCalled = true; }
+      var errorCalled = false;
+      function error() { errorCalled = true; }
+      var cb = belhop.factory.callback(success, error);
+
+      cb.success();
+      expect(successCalled).toBe(true);
+
+      cb.error();
+      expect(errorCalled).toBe(true);
+    });
+
+    it('can no-op errors', function() {
+      function success() { }
+      var cb = belhop.factory.callbackNoErrors(success);
+      expect(cb.success).toBe(success);
+      expect(typeof cb.error).toBe('function');
+      expect(cb.error()).not.toBeDefined();
+     });
+
+    it('can no-op success', function() {
+      function error() { }
+      var cb = belhop.factory.callbackNoSuccess(error);
+      expect(cb.error).toBe(error);
+      expect(typeof cb.success).toBe('function');
+      expect(cb.success()).not.toBeDefined();
+     });
 
   });
 
