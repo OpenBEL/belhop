a89f96e96b13655e9edd1d5df5a6bd7184601781
Nick Bargnesi <nbargnesi@selventa.com>
2015-02-24 19:19:28 -0500

    ignore additional jsdoc tags

--- scripts/gjslint.sh
+++ scripts/gjslint.sh
@@ -19,7 +19,7 @@ export GOSH_CONTRIB_PYTHON_VENV="$GJSLINT_ENV"
 create_python_env "python2"
 
 JSDOC_TAGS="function,namespace,default,example,property,name,file,exports,
-version,readonly,tutorial,memberof"
+version,readonly,tutorial,memberof,instance,method,augments"
 
 cd "$DIR" || exit 1
 require-cmd-or-die "gjslint"
